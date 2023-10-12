59153ef06b88f9cd50e9426d25fb9af5edff4909
Eero Kokkonen <eero.kokkonen@hotmail.com>
2023-10-12 14:16:04 +0300

    Database muutokset

--- db/init.sql
+++ db/init.sql
@@ -8,13 +8,5 @@ CREATE TABLE IF NOT EXISTS accounts (
     last_login TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS plants (
-	plant_id varchar ( 36)  UNIQUE NOT NULL,
-	name VARCHAR ( 50 ) UNIQUE NOT NULL,
-	description VARCHAR ( 1000 ) NOT NULL,
-	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-	created_on TIMESTAMP NOT NULL,
-    last_login TIMESTAMP
-);
 INSERT INTO accounts(username, password, email, created_on, last_login)
 VALUES ('admin','admin','admin@admin.com', '1999-01-08 04:05:06', '1999-01-08 04:05:06');
