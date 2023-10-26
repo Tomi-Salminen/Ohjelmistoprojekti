6b2f43b0cfc90aa4948a212827a47d838e8732df
Marika Verger <marika.verger@tuni.fi>
2023-10-26 14:31:53 +0300

    lisää api endpointseja

--- backend/controllers/plants.js
+++ backend/controllers/plants.js
@@ -1,4 +1,4 @@
-//const Joi = require('joi');
+const Joi = require('joi');
 const plants = require('../models/plants');
 
 //get all the plants
@@ -28,8 +28,109 @@ const getPlantsById = async (req, res) => {
     }
   };
 
+//create a plant
+const createPlant = async (req, res) => {
+  //validate the information using Joi
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.string().required(),
+    image: Joi.string()
+  });
+  //console.log(req.body);
+  
+  const {error} = schema.validate(req.body);
+  if(error){
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const plant = {
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    image: req.body.image
+  };
+
+  try {
+    const response = await plants.create(plant);
+    console.log(response);
+    if (response) {
+      res.status(201).send(plant);
+    }
+
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+};
+
+
+//update existing plant
+const updatePlant = async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  //validate information
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.string().required(),
+    image: Joi.string()
+  });
+  //console.log(req.body);
+  
+  const {error} = schema.validate(req.body);
+  if(error){
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const plant = {
+    id: id,
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    image: req.body.image
+  };
+  
+  try {
+    const result = await plants.findPlantsById(id);
+    if (result.length === 0) {
+      res.status(404).send('Not Found');
+      return;
+    }
+    const response = await plants.updateById(plant);
+    if (response) {  
+      res.status(200).send("Plant information updated");
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+};
+
+//delete plant by id
+const deletePlant = async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  try {
+    const result = await plants.findPlantsById(id);
+    if (result.length === 0) {
+      res.status(404).send('Not Found');
+      return;
+    }
+    const response = await plants.deleteById(id);
+    if(response) {
+      res.status(200).send("Plant deleted");
+    }
+    
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  } 
+};
+
 
 module.exports = {
     getPlants,
     getPlantsById,
+    createPlant,
+    updatePlant,
+    deletePlant,
   };
