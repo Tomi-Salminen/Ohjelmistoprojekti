6d2681950d477c5353cd7eef63e232b2909c90ab
Eero Kokkonen <eero.kokkonen@hotmail.com>
2023-10-31 16:59:53 +0200

    Tokenin validointi mahdollisuus lisätty

--- backend/routes/plants.js
+++ backend/routes/plants.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../middleware/verifyToken");
 
 const {
   getPlants,
--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE IF NOT EXISTS accounts (
-  user_id varchar(36) UNIQUE NOT NULL,
+  user_id varchar(36) UNIQUE NOT NULL PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
   password VARCHAR(60) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
