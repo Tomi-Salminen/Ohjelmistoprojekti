3a1f0328769b3ce4e3bdd24c902b9648a6ef0f28
Eero Kokkonen <eero.kokkonen@hotmail.com>
2023-11-28 13:02:31 +0200

    DB connection tietojen vienti env tiedostoon

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,6 @@ require("dotenv").config();
 
 const cors = require("cors");
 
-//const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 // Cors asetukset
@@ -23,6 +21,5 @@ app.use(bodyParser.json());
 app.use("/api/accounts", accountRoutes);
 app.use("/api/plants", plantsRoutes);
 app.use("/api/orders", ordersRoutes);
-//app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 module.exports = app;
